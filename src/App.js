ad0c84c9da97631633963b21a57b780fb10fd78d
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-06-18 21:24:42 +0530

    initialize create-react-app project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { BiArchive } from "react-icons/bi";
+
+function App() {
+	return (
+		<div className="App container mx-auto mt-3 font-thin">
+			<h1 className="text-5xl">
+				<BiArchive className="inline-block text-red-500 align-top" />
+				My Appointments
+			</h1>
+		</div>
+	);
+}
+
+export default App;
