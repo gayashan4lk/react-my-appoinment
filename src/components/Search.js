28abd741c7a9548754c5518f1615e85abdf5862e
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-06-19 11:52:57 +0530

    minor style fix

--- src/components/Search.js
+++ src/components/Search.js
@@ -72,7 +72,7 @@ const Search = () => {
 								setToggleDropDown(!toggleDropDown);
 							}}
 							type="button"
-							className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center"
+							className="justify-center px-4 py-1.5 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center rounded-r-md"
 							id="options-menu"
 							aria-haspopup="true"
 							aria-expanded="true"
